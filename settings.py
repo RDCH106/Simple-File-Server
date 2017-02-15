0f4f7093d49dfb4d8010b50dbf1602c55b6c935e
Rubén de Celis Hernández <rubendecelis@ingran.es>
2017-02-15 12:47:09 +0100

    Updated default settings

--- settings.py
+++ settings.py
@@ -2,8 +2,8 @@
 # after running `sudo ./install`, the server should run at http://host:port/base_url
 host = '127.0.0.1'
 port = 5000
-base_url = 'some_preferred_url/'
+base_url = 'files'
 username = 'admin'
 password = 'mypassword'
 
-# for example, with above default values, the server should run at 127.0.0.1:5000/some_preferred_url/
+# for example, with above default values, the server should run at 127.0.0.1:5000/files
